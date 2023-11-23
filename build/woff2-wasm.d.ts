91ef55f60d0ef09ca5f60dc6ce3160ad47a44115
Kyedo <31018128+itskyedo@users.noreply.github.com>
2023-11-22 23:49:29 -0800

    fix: load module on onRuntimeInitialized event (#6)

--- build/woff2-wasm.d.ts
+++ build/woff2-wasm.d.ts
@@ -1,4 +1,8 @@
-export default function initModule(): Promise<{
+export interface ModuleArgs {
+  onRuntimeInitialized?: () => void;
+}
+
+export default function initModule(args?: ModuleArgs): Promise<{
   compress(buffer: ArrayBuffer | Uint8Array): Promise<Uint8Array | null>;
   decompress(buffer: ArrayBuffer | Uint8Array): Promise<Uint8Array | null>;
 }>;
